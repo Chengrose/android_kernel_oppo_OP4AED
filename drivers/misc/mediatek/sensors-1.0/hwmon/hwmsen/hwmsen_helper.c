ef7226e191d85bab325430acf52611cb77878cc8
Chengrose <3485605154@qq.com>
2023-11-19 16:13:39 +0800

    改回memcmp

--- drivers/misc/mediatek/sensors-1.0/hwmon/hwmsen/hwmsen_helper.c
+++ drivers/misc/mediatek/sensors-1.0/hwmon/hwmsen/hwmsen_helper.c
@@ -361,7 +361,7 @@ void hwmsen_multi_rw(struct i2c_client *client, find_reg_t findreg,
 				continue;
 			}
 
-			if (bcmp(buf, dat, len)) {
+			if (memcmp(buf, dat, len)) {
 				pr_debug("(%d) data compare fail!!\n", idx);
 				pr_debug("buf:");
 				for (p = 0; p < len; p++)
