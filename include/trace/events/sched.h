bc6e6165714ffa66faa2881d25d73983f49cd512
Chengrose <3485605154@qq.com>
2024-03-07 19:40:49 +0800

    Update sched.h

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -1,10 +1,11 @@
+#include <linux/sched.h>
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM sched
 
 #if !defined(_TRACE_SCHED_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_SCHED_H
 
-#include <linux/sched.h>
+
 #include <linux/tracepoint.h>
 #include <linux/binfmts.h>
 
